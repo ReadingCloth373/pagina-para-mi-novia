8155fc2a1b672f3b2481fbd76c92a9ce780325c3
ReadingCloth373 <oscar2008ch@gmail.com>
2025-11-03 21:25:43 -0600

    Add files via upload

---
+++ j.js
@@ -0,0 +1,8 @@
+function Perdiste(){
+    document.write("<h1 style='color: rgb(247, 12, 24)'>El juego ha terminado </h1>");
+    document.write("<img src='imagenes/perdiste.gif' alt='perder'>");
+}
+function Ganaste(){
+    document.write("<h1 style='color: rgb(45, 184, 10)'>Ganaste el juego</h1>");
+    document.write("<img src='imagenes/ganaste.png' alt='bien'>");
+}
